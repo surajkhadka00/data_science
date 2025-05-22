70ab95f8374fd273f35f7dd110697c786172bc19
suraj khadka <surajmkhadka@gmail.com>
2025-05-22 13:29:59 +0545

    the is the assignmnet

---
+++ Day_2/day2assignment/areaofcircle.py
@@ -0,0 +1,8 @@
+import math
+def value(r):
+    return math.pi*r**2
+r=int(input("enter a radious"))
+print(f"the area a of a circle is{value(r):.2f}meter square")
+
+
+
---
+++ Day_2/day2assignment/c=2pir.py
@@ -0,0 +1,5 @@
+import math
+def value(r):
+    return 2*math.pi*r
+r=int(input("enter a radious"))
+print(f"the curcufarance of a ccircleis:{value(r):.2f} meter square")
